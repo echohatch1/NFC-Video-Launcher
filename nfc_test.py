c4c8d1c63d1350ae26e400644d04b728c78f9ae8
Landon Hatch <landonhatch@gmail.com>
2019-10-21 22:38:28 -0600

    Added excape route

--- nfc_test.py
+++ nfc_test.py
@@ -3,81 +3,54 @@ from py532lib.frame import *
 from py532lib.constants import *
 import time
 import os
-
-#imports for openCV implementation
+from threading import Thread
 import cv2
 import numpy as np
 
+#OpenCV Implementation
+def open_img():
+    img = cv2.imread("recycling_home.png")
+
+    cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+    
+    cv2.imshow("window", img)
+    
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    
 #display video after scanning
 def open_vid(vid):
     command = "omxplayer " + vid
     os.system(command)
-
-
-#OpenCV Implementation
-
-def open_img():
-    # Create a VideoCapture object and read from input file
-    # If the input is the camera, pass 0 instead of the video file name
-    cap = cv2.VideoCapture('recycling_home.png')
-
-    # Check if camera opened successfully
-    if (cap.isOpened()== False):
-        print("Error opening video stream or file")
-
-    # Read until video is completed
-    while(cap.isOpened()):
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        if ret == True:
-
-            # Display the resulting frame
-            #cv2.imshow('Frame',frame)
-
-            #Display above but full-screen
-            cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
-            cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
-            cv2.imshow("window", frame)
-
-
-            # Press Q on keyboard to  exit
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-
-        # Break the loop
-        else:
-            break
-
-
     
-open_img()
-
-
+#listen for nfc cards    
 def read_card():
+    while True:
+        pn532 = Pn532_i2c()
+        pn532.SAMconfigure()
 
-    pn532 = Pn532_i2c()
-    pn532.SAMconfigure()
-
-    card_data = pn532.read_mifare().get_data()
+        card_data = pn532.read_mifare().get_data()
 
 
-    print(card_data)
+        print(card_data)
 
-    cardOne = bytearray(b'K\x01\x01\x00\x04\x08\x04W \xdeR')
-    cardTwo = bytearray(b'K\x01\x01\x00\x04\x08\x04k\x90]\x1b')
+        cardOne = bytearray(b'K\x01\x01\x00\x04\x08\x04W \xdeR')
+        cardTwo = bytearray(b'K\x01\x01\x00\x04\x08\x04k\x90]\x1b')
 
-    if card_data == cardOne:
-        print("card 1")
-        open_vid("sample.mp4")
+        if card_data == cardOne:
+            print("card 1")
+            open_vid("sample.mp4")
 
-    elif card_data == cardTwo:
-        print("card 2")
-        open_vid("trash.mp4")
+        elif card_data == cardTwo:
+            print("card 2")
+            open_vid("trash.mp4")
 
-    else:
-        print("Card not recognized")
-        time.sleep(5)
+        else:
+            print("Card not recognized")
+            time.sleep(5)
 
-i = True
-while i:
-    read_card()
+
+if __name__ == '__main__':
+    Thread(target = open_img).start()
+    Thread(target = read_card).start()
