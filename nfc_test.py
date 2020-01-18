fb4cac00739e9dc4b3b55291137d64a26c94a7c8
Landon Hatch <landonhatch@gmail.com>
2020-01-18 15:41:55 -0700

    Added json file to store card data in

--- nfc_test.py
+++ nfc_test.py
@@ -6,6 +6,7 @@ import os
 from threading import Thread
 import cv2
 import numpy as np
+import json
 
 #OpenCV Implementation
 def open_img():
@@ -26,29 +27,44 @@ def open_vid(vid):
     
 #listen for nfc cards    
 def read_card():
+    
+    #convert bytearray to hex
+    #cardOneHex = cardOne.hex()
+    
+    #convert hex to bytearray
+    #bytearray.fromhex(cardOneHex)
+    
+    #get card data from json file (hex format)
+    data = json.load(open('cardData.json'))
+    
+    #store data from json files into variables and convert from hex format
+    cardOne = bytearray.fromhex(data["cardOne"])
+    cardTwo = bytearray.fromhex(data["cardTwo"])
+    
+    #cardOne = bytearray(b'K\x01\x01\x00\x04\x08\x04W \xdeR')
+    #cardTwo = bytearray(b'K\x01\x01\x00\x04\x08\x04k\x90]\x1b')
+        
     while True:
         pn532 = Pn532_i2c()
         pn532.SAMconfigure()
 
         card_data = pn532.read_mifare().get_data()
+        
+        if card_data != bytearray(b'K\x00'):
+            
+            print(card_data)
+        
+            if card_data == cardOne:
+                print("card 1")
+                open_vid("sample.mp4")      
 
+            elif card_data == cardTwo:
+                print("card 2")
+                open_vid("trash.mp4")
 
-        print(card_data)
-
-        cardOne = bytearray(b'K\x01\x01\x00\x04\x08\x04W \xdeR')
-        cardTwo = bytearray(b'K\x01\x01\x00\x04\x08\x04k\x90]\x1b')
-
-        if card_data == cardOne:
-            print("card 1")
-            open_vid("sample.mp4")
-
-        elif card_data == cardTwo:
-            print("card 2")
-            open_vid("trash.mp4")
-
-        else:
-            print("Card not recognized")
-            time.sleep(5)
+            else:
+                print("Card not recognized")
+                time.sleep(5)
 
 
 if __name__ == '__main__':
